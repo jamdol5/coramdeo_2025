c43c5a0ae787dcb23641db5e4e9fcd9cdf17d1a6
jamdol5 <78344248+jamdol5@users.noreply.github.com>
2024-09-06 21:30:02 -0700

    page6_3.py

--- page6_3.py
+++ page6_3.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import date, datetime, timedelta
 from dateutil.relativedelta import relativedelta
-import db_utils  # Import the db_utils module
+from db_utils import save_attendance_to_db, get_attendance_summary  # Import specific functions
 
 def app():
     custom_css = """
@@ -35,31 +35,30 @@ def app():
     
     st.markdown(custom_css, unsafe_allow_html=True)
 
-     # Initialize the database
-    db_utils.init_db()
-    # df = pd.read_excel(r'/Users/hyungju/Documents/Coding/Langchain/2024-2025 연락처.xlsx',sheet_name='Small Group_스몰그룹')
+    # Get the database connection from session state
+    conn = st.session_state.db_connection
 
     # Define the names directly in a dictionary
     names_dict = {
         "6학년 남자": ["권예준","김지훈","박시후","신준민","정라온"]
     }
-    
+
+    # Create the Roster dictionary dynamically
     Roster = {grade: [{"label": name, "default":False} for name in names] for grade, names in names_dict.items()}
-    
+
     # Initialize attendance tracking
     at = {grade: {} for grade in names_dict.keys()}
 
     # Create a Streamlit expander and display the values
     st.title('Student Roster')
+    
     for grade, students in Roster.items():
         with st.expander(grade):
             for student in students:
                 at[grade][student["label"]] = st.checkbox(student["label"], key=student["label"],value=True)
-
-
-     # Button to save attendance data
+    
+    # Button to save attendance data
     if st.button("Save Attendance"):
         date = datetime.now().strftime('%Y-%m-%d')
-        db_utils.save_attendance_to_db(at, date)
+        save_attendance_to_db(conn, at, date)
         st.success("Attendance data saved successfully!")
-
