009757ff46689d8453dbf31eb7cdc1238ad26601
jamdol5 <78344248+jamdol5@users.noreply.github.com>
2024-09-06 21:45:26 -0700

    page9_3.py

--- page9_3.py
+++ page9_3.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import date, datetime, timedelta
 from dateutil.relativedelta import relativedelta
-import db_utils  # Import the db_utils module
+from db_utils import save_attendance_to_db, get_attendance_summary  # Import specific functions
 
 def app():
     custom_css = """
@@ -35,9 +35,10 @@ def app():
     
     st.markdown(custom_css, unsafe_allow_html=True)
 
-    # Initialize the database
-    db_utils.init_db()
+    # Get the database connection from session state
+    conn = st.session_state.db_connection
 
+    # Define the names directly in a dictionary
     names_dict = {
         "9학년 여자": ["박정현","김지아","박지효","박지빈","박서현","박예림","성다애","임지윤","최윤지"]
     }
@@ -50,15 +51,14 @@ def app():
 
     # Create a Streamlit expander and display the values
     st.title('Student Roster')
-
+    
     for grade, students in Roster.items():
         with st.expander(grade):
             for student in students:
                 at[grade][student["label"]] = st.checkbox(student["label"], key=student["label"],value=True)
     
-
-     # Button to save attendance data
+    # Button to save attendance data
     if st.button("Save Attendance"):
         date = datetime.now().strftime('%Y-%m-%d')
-        db_utils.save_attendance_to_db(at, date)
+        save_attendance_to_db(conn, at, date)
         st.success("Attendance data saved successfully!")
