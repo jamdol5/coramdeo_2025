cd5eda554c89ebb7165cc09e0334f0264dc362d4
jamdol5 <78344248+jamdol5@users.noreply.github.com>
2024-09-07 22:47:22 -0700

    page7_2.py

--- page7_2.py
+++ page7_2.py
@@ -40,7 +40,7 @@ def app():
 
     # Define the names directly in a dictionary
     names_dict = {
-        "7학년 여자": ["윤채이","박주은","아혜인","김하민","김미소","주은서","김소망","이민하"]
+        "7학년 여자": ["김미소","김소망","김하민","아혜인","유안나","윤채이","이민하","주은서"]
     }
 
     # Create the Roster dictionary dynamically
